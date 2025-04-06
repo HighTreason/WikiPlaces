c7f964b271372281bfa844c20b0fedbb78714431
Charles Abou Yakdan <charles@Charless-MacBook-Pro.local>
2025-04-06 19:05:46 +0200

    Add the initial project files

---
+++ WikiPlaces/WikiPlacesApp.swift
@@ -0,0 +1,17 @@
+//
+//  WikiPlacesApp.swift
+//  WikiPlaces
+//
+//  Created by Charles Abou Yakdan on 06/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct WikiPlacesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WikiPlacesTests/WikiPlacesTests.swift
@@ -0,0 +1,16 @@
+//
+//  WikiPlacesTests.swift
+//  WikiPlacesTests
+//
+//  Created by Charles Abou Yakdan on 06/04/2025.
+//
+
+import Testing
+
+struct WikiPlacesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
